fb9999cf70553e505a2159f4e58d7cb7b6ba21e1
TruongIKPK <phitruong20192020@gmail.com>
2026-04-19 22:51:51 +0700

    first commit

---
+++ src/main/java/com/hamtech/bookstorenewsservice/BookstoreNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorenewsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreNewsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreNewsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamtech/bookstorenewsservice/BookstoreNewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorenewsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreNewsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
